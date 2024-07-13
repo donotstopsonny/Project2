1f684043d687a9fcdc2f0e008100ebb653e263ba
Hyeongchae Son <3076270s@gmail.com>
2024-07-14 05:27:13 +0900

    Initial Commit

---
+++ Project2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Project2
+//
+//  Created by Hyeongchae Son on 7/14/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
